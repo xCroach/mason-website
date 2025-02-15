9f23fd3d5df7bca5fc251ec29ef2ad10f2ed82b4
Croach <44575914+xCroach@users.noreply.github.com>
2025-02-15 13:49:47 +0100

    Adjust router.js

--- src/router.js
+++ src/router.js
@@ -5,8 +5,8 @@ import Projects from './components/Projects.vue';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/about', component: About },
-    { path: '/projects', component: Projects }
+    { path: '/components/about', component: About },
+    { path: '/components/projects', component: Projects }
 ];
 
 const router = createRouter({
