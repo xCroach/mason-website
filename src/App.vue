e00bb85deae60931a7b93c18e2c955b29cf2c318
Lukas Heinzl, BSc <l.heinzl.stp@gmail.com>
2025-02-25 17:04:11 +0100

    Add new menubar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+  <Menubar :model="items"/>
+
+  <main>
+    <router-view/>
+  </main>
 </template>
 
+<script setup>
+import {ref} from "vue";
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    url: '/'
+  },
+  {
+    label: 'About',
+    icon: 'pi pi-user',
+    url: '/about'
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-folder',
+    url: '/projects'
+  },
+]);
+</script>
+
 <script>
+import Menubar from 'primevue/menubar';
 
 export default {
   name: 'App',
-  components: { },
-  setup(){
+  components: {Menubar},
+  setup() {
     return {}
   }
 };
@@ -17,4 +42,3 @@ export default {
 
 <style scoped>
 </style>
-
--- src/router.js
+++ src/router.js
@@ -5,8 +5,8 @@ import Projects from './components/Projects.vue';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/components/about', component: About },
-    { path: '/components/projects', component: Projects }
+    { path: '/about', component: About },
+    { path: '/projects', component: Projects }
 ];
 
 const router = createRouter({
